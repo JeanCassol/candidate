015d39b9fe0ea641f6abedf163e1019f0ac9eaf0
LeonardoCatelan <44733148+LeonardoCatelan@users.noreply.github.com>
2018-11-17 04:44:35 -0200

    pre 0.3

--- src/main/java/br/edu/ulbra/election/candidate/exception/GenericOutputException.java
+++ src/main/java/br/edu/ulbra/election/candidate/exception/GenericOutputException.java
@@ -1,14 +1,8 @@
 package br.edu.ulbra.election.candidate.exception;
 
-
 public class GenericOutputException extends RuntimeException {
 
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public GenericOutputException(String message){
+    public GenericOutputException(String message){
         super(message);
     }
 }
--- src/main/java/br/edu/ulbra/election/candidate/model/Candidate.java
+++ src/main/java/br/edu/ulbra/election/candidate/model/Candidate.java
@@ -2,71 +2,63 @@ package br.edu.ulbra.election.candidate.model;
 
 import javax.persistence.*;
 
-/*
- *  id integer identity primary key,
-  name varchar(255) not null,
-  number integer not null,
-  election_id integer not null,
-  party_id integer not null
-  
- */
 @Entity
 public class Candidate {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false)
-	private String name;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false, name = "number")
-	private Long numberElection;
+    @Column(nullable = false)
+    private Long numberElection;
 
-	@Column(nullable = false, name = "election_id")
-	private Long electionId;
+    @Column(nullable = false)
+    private Long electionId;
 
-	@Column(nullable = false, name = "party_id")
-	private Long partyId;
+    @Column(nullable = false)
+    private Long partyId;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Long getNumberElection() {
-		return numberElection;
-	}
+    public Long getNumberElection() {
+        return numberElection;
+    }
 
-	public void setNumberElection(Long numberElection) {
-		this.numberElection = numberElection;
-	}
+    public void setNumberElection(Long numberElection) {
+        this.numberElection = numberElection;
+    }
 
-	public Long getElectionId() {
-		return electionId;
-	}
+    public Long getElectionId() {
+        return electionId;
+    }
 
-	public void setElectionId(Long electionId) {
-		this.electionId = electionId;
-	}
+    public void setElectionId(Long electionId) {
+        this.electionId = electionId;
+    }
 
-	public Long getPartyId() {
-		return partyId;
-	}
+    public Long getPartyId() {
+        return partyId;
+    }
 
-	public void setPartyId(Long partyId) {
-		this.partyId = partyId;
-	}
+    public void setPartyId(Long partyId) {
+        this.partyId = partyId;
+    }
 
 }
